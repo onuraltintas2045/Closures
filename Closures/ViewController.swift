4a6817038d7bc69185eeb0c77ae7b4179aaa86d3
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-05-12 23:54:21 +0300

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Onur Altintas on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
