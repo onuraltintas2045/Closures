a25f5abe4e9e826ca59ec6a6cc94622b6d6474c3
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-05-13 23:30:14 +0300

    The folder process is complete

---
+++ Closures/Views/ClosuresViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClosuresViewController.swift
+//  Closures
+//
+//  Created by Onur Altintas on 13.05.2025.
+//
+
+import UIKit
+
+class ClosuresViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+
+}
